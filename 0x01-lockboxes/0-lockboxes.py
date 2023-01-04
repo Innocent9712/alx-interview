86ddcc60dd6fe3f586c287a12af35c89c87bb65d
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-04 19:27:09 +0100

    Remove logging

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -11,7 +11,6 @@ def canUnlockAll(boxes):
         return False
     keys = [0]
     for key in keys:
-        print(key)
         for new_key in boxes[key]:
             if new_key not in keys and new_key < len(boxes):
                 keys.append(new_key)
