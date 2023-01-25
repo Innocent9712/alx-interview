758da5e2c5286cd96a7b51613732f52b00ae275a
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-25 19:18:01 +0100

    Add task

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,33 +1,52 @@
 #!/usr/bin/python3
-"""0. UTF-* Validation"""
+"""
+Define validUTF8(data) function that validates whether a
+string of ints represents a valid UTF-8 encoding.
+"""
+from itertools import takewhile
 
 
-def decimalToBinary(dps):
-    return [bin(dp).replace("0b", "") for dp in dps]
+def int_to_bits(nums):
+    """
+    Helper function
+    Convert ints to bits
+    """
+    for num in nums:
+        bits = []
+        mask = 1 << 8  # cause we have 8 bits per byte. adds up to (11111111)
+        while mask:
+            mask >>= 1
+            bits.append(bool(num & mask))
+        yield bits
 
 
 def validUTF8(data):
-    """a method that determines if a given data
-    set represents a valid UTF-8 encoding.
     """
-    datapoints = decimalToBinary(data)
-    i = 0
-    while i < len(datapoints):
-        bnum = datapoints[i]
-        while len(bnum) < 8:
-            bnum = '0' + bnum
-        if bnum[0] == '1':
-            j = 0
-            count = 0
-            while bnum[j] == '1':
-                count += 1
-                j += 1
-            q = i + 1
-            for w in range(count):
-                if not datapoints[q].startswith('10'):
-                    return False
-                q += 1
-            i = q
-        else:
-            i += 1
+    Takes a list of ints and returns true if the list is
+    a valid UTF-8 encoding, else returns false
+    Args:
+        data : List of ints representing possible UTF-8 encoding
+    Return:
+        bool : True or False
+    """
+    bits = int_to_bits(data)
+    for byte in bits:
+        # if single byte char, then valid. continue
+        if byte[0] == 0:
+            continue
+
+        # if here, byte is multi-byte char
+        ones = sum(takewhile(bool, byte))
+        if ones <= 1:
+            return False
+        if ones >= 4:  # UTF-8 can be 1 to 4 bytes long
+            return False
+
+        for _ in range(ones - 1):
+            try:
+                byte = next(bits)
+            except StopIteration:
+                return False
+            if byte[0:2] != [1, 0]:
+                return False
     return True
