9d67dc49a01a2c25e6d9a4a6bfa85521efc03a23
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-01-31 09:46:24 +0100

    cleanup

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -20,50 +20,33 @@ if layout < 4:
 
 
 def check_here(nlist, index, curr_num):
-    # print("running check here")
     for item in nlist:
         if index == item[1] or\
                 index == item[1] - (curr_num - item[0]) or\
                 index == item[1] + (curr_num - item[0]):
             return 1
-    # print(f"adding: [{curr_num, index}]")
     return list([curr_num, index])
 
 
 def loop_n_find(nlist, curr_num):
-    # print("running recurse", curr_num)
-    # print(curr_num, layout)
     if (curr_num > layout):
         return
 
     if len(nlist) == layout:
-        # print("num found")
-        # print(curr_num, layout)
         print(nlist)
         return
 
     j = 0
     while j < layout:
         val = check_here(nlist, j, curr_num)
-        # if val == 1:
-        #     continue
-        # print(type(val))
-        # print("val here",val)
         if type(val) is list:
-            # print("list fo")
-            # nlist.append(val)
-            # print("curr_list:", nlist)
             new_nlist = nlist.copy()
             new_nlist.append(val)
-            # print("new list",new_nlist)
             loop_n_find(new_nlist, curr_num + 1)
-            # break
         j += 1
-    # print("done here")
     return
 
 
 for i in range(layout):
     arr = [[0, i]]
     loop_n_find(arr, 1)
-    # print("done here")
