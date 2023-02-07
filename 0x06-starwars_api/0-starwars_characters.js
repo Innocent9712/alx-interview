a2ab90ae775daa3ec7ef10c557f6e1e88e920399
Innocent Chukwuemeka <chukwuemeka140@gmail.com>
2023-02-07 09:04:49 +0100

    Fix requirement

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -6,14 +6,26 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (erro
     console.log(error);
   } else {
     const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, function (error, response, body) {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+    // for (const character of characters) {
+    //   request(character, function (error, response, body) {
+    //     if (error) {
+    //       console.log(error);
+    //     } else {
+    //       console.log(JSON.parse(body).name);
+    //     }
+    //   });
+    // }
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (err, response, body) {
+    if (!err) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
